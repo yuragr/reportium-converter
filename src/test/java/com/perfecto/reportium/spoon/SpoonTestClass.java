c8b312002c5effa29f16a00a0f3513436abc0a2d
Yuri Granovsky <yurig@perfectomobile.com>
2017-03-22 11:59:35 +0200

    add scan

--- src/test/java/com/perfecto/reportium/spoon/SpoonTestClass.java
+++ src/test/java/com/perfecto/reportium/spoon/SpoonTestClass.java
@@ -1,9 +1,11 @@
 package com.perfecto.reportium.spoon;
 
+import java.util.ArrayList;
+
 /**
  * Created by yurig on 22-Mar-17.
  */
-public class SpoonTestClass {
+public class SpoonTestClass extends ArrayList<String> {
     public int i;
 
     public void test() {
