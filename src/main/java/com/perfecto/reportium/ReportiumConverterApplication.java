de5813bde5b6c3142380338843b8758e23e988cf
Yuri Granovsky <yurig@perfectomobile.com>
2017-03-22 08:10:09 +0200

    initial commit

---
+++ src/main/java/com/perfecto/reportium/ReportiumConverterApplication.java
@@ -0,0 +1,12 @@
+package com.perfecto.reportium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportiumConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportiumConverterApplication.class, args);
+	}
+}
